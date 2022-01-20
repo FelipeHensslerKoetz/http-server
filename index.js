503ba58485f5ee2b9ebab53f65d50e8a4a858ad1
Felipe Henssler Koetz <felipehenssler@gmail.com>
2022-01-19 21:38:19 -0300

    feat: primitive http server finished

--- index.js
+++ index.js
@@ -1,21 +1,33 @@
 const http = require("http");
 const PORT = 3000;
 
+const names = [
+  { id: 1, name: "Issac Newton" },
+  { id: 2, name: "Nicolaus Copernicus" },
+  { id: 3, name: "NIkola Tesla" },
+  { id: 4, name: "Albert Einstein" },
+];
+
 const server = http.createServer((req, res) => {
-  if (req.url === "/name") {
+  const items = req.url.split("/");
+
+  if (req.method === "POST" && items[1] === "names") {
+    req.on("data", (data) => {
+      const name = data.toString();
+      names.push(JSON.parse(name));
+    });
+
+    req.pipe(res);
+  } else if (req.method === "GET" && items[1] === "name") {
     res.setHeader("Content-Type", "application/json");
     res.end(JSON.stringify({ name: "Felipe Henssler Koetz" }));
-  } else if (req.url === "/names") {
-    res.setHeader("Content-Type", "text/html");
-    res.write("<html>");
-    res.write("<body>");
-    res.write("<ul>");
-    res.write("<li> Hello Felipe </li>");
-    res.write("<li> Hello Felipe 2 </li>");
-    res.write("</ul>");
-    res.write("</body>");
-    res.write("</html>");
-    res.end();
+  } else if (req.method === "GET" && items[1] === "names") {
+    if (items.length === 3) {
+      const index = Number(items[2]) - 1;
+      res.end(JSON.stringify(names[index]));
+    } else {
+      res.end(JSON.stringify(names));
+    }
   } else {
     res.statusCode = 404;
     res.end();
